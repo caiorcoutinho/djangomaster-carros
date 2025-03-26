ea28b69884f37c9bd7fb2e9c4f3d10bf00c1f2b6
caio <caiorcoutinho16@gmail.com>
2025-03-26 00:05:01 -0300

    change admin data view

--- cars/admin.py
+++ cars/admin.py
@@ -10,7 +10,7 @@ admin.site.register(Brand, BrandAdmin)
 
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value',)
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value', 'photo', 'plate',)
     search_fields = ('model', 'brand',)
 
 admin.site.register(Car, CarAdmin)
