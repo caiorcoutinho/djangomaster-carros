bbfbacd66096e36e3e06fcfb1c2a1a3d776390c8
caio <caiorcoutinho16@gmail.com>
2025-03-25 21:40:31 -0300

    implements image uploads

--- cars/admin.py
+++ cars/admin.py
@@ -5,8 +5,6 @@ class BrandAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
 
-    def __str__(self):
-        return self.name
     
 admin.site.register(Brand, BrandAdmin)
 
--- cars/models.py
+++ cars/models.py
@@ -4,6 +4,9 @@ class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,6 +15,8 @@ class Car(models.Model):
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    plate = models.CharField(max_length=9, blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self):
         return self.model
