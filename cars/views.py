7bf8039b88472166463b85c2645e748c64d27ef2
= <caiorcoutinho16@gmail.com>
2025-04-11 18:05:04 -0300

    crud completo

--- cars/views.py
+++ cars/views.py
@@ -2,22 +2,75 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.db.models import Q
+from django.views import View
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
-def new_car_view(request):
-    if request.method == "POST":
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+    
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
-    if search:
-        cars = cars.filter(brand__name__icontains=search, model__icontains=search)
-    
-    return render(request, 'cars.html', { 'cars': cars })
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/'
+
+class CarsView(View):
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        search = request.GET.get('search')
+
+        if search:
+            cars = cars.filter(
+                Q(brand__name__icontains=search) | Q(model__icontains=search) | Q(factory_year__contains=search) | Q(model_year__contains=search)
+            )
+        
+        return render(
+            request, 
+            'cars.html', 
+            { 'cars': cars },
+        )
+
+
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(
+                Q(brand__name__icontains=search) | Q(model__icontains=search) | Q(factory_year__contains=search) | Q(model_year__contains=search)
+            )
+        return cars
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = "/"
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = "/"
